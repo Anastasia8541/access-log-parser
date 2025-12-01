233d040cca6a0974b6c1c91d315b73b0bb095264
Anastasia N <git.user.nashko2013@yandex.ru>
2025-12-01 22:42:03 +0300

    Практика арифметических операций

--- src/main/java/com/stepup/Main.java
+++ src/main/java/com/stepup/Main.java
@@ -1,17 +1,18 @@
 package com.stepup;
 
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        System.out.println("Введите число: ");
+        int number = new Scanner(System.in).nextInt();
+        System.out.println("Введите число: ");
+        int number2 = new Scanner(System.in).nextInt();
+        System.out.println(number+number2);
+        System.out.println(number-number2);
+        System.out.println(number*number2);
+        System.out.println((double)number/(double)number2);
     }
 }
